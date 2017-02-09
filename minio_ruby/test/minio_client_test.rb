571d9ce7921c16424c627ed69b4c25ab378cf037
Dee Koder <deekoder@users.noreply.github.com>
2017-02-08 19:10:15 -0800

    updated text.

--- minio_ruby/test/minio_client_test.rb
+++ minio_ruby/test/minio_client_test.rb
@@ -2,12 +2,12 @@ require 'minio_ruby'
 require 'open-uri'
  
 
-mc = MinioRuby::MinioClient.new(endPoint:"https://s3.amazonaws.com", accessKey:"AKIAITFCLT5MNJ3C6CNA" , secretKey:"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", region: "us-east-1" )
-puts mc.getObject("deeruby","ip_table.png")
+mc = MinioRuby::MinioClient.new(endPoint:"https://s3.amazonaws.com", accessKey:"" , secretKey:"", region: "us-east-1" )
+puts mc.getObject("mybucket","myimage.png")
 
 file = open("hello.txt").read
-mc.putObject("deeruby", "tfile.txt", file, file.size, 'text/plain')
+mc.putObject("mybucket", "tfile.txt", file, file.size, 'text/plain')
 
-mc.getObject("deeruby","tfile.txt")
+mc.getObject("mybucket","tfile.txt")
 
  
